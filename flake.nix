62776c1aa58fb2543a5fcff8af2eb82cee27ae66
Kat Inskip <kat@inskip.me>
2024-10-09 15:03:53 -0400

    feat: remove excess

--- flake.nix
+++ flake.nix
@@ -25,37 +25,10 @@
           pkgs.callPackage ./packages/scalpel.nix {
             inherit matchers source destination user group mode;
           };
+       }) // {
 
         nixosModules.scalpel = import ./modules/scalpel {inherit self;};
         nixosModule = self.nixosModules.scalpel;
 
-        nixosConfigurations = let
-          base_sys = nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              sops-nix.nixosModules.sops
-              ./example/system.nix
-            ];
-          };
-        in {
-          exampleContainer = base_sys.extendModules {
-            modules = [
-              self.nixosModules.scalpel
-              ./example/scalpel.nix
-            ];
-            specialArgs = {prev = base_sys;};
-          };
-
-          exampleContainerManual = base_sys.extendModules {
-            modules = [
-              ./example/scalpel-manual.nix
-            ];
-            specialArgs = {
-              prev = base_sys;
-              inherit (self) mk_scalpel;
-            };
-          };
         };
-      }
-    );
 }
